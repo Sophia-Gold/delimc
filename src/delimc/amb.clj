e6320e9e10d516f67166be046e7025e7e11360c8
Sophia Gold <sophiagoldnyc@gmail.com>
2018-04-19 04:50:41 -0400

    amb

--- src/delimc/amb.clj
+++ src/delimc/amb.clj
@@ -1,3 +1,26 @@
 (ns delimc.amb
   (:require [delimc.core :refer :all]))
 
+(def fail (atom (println "amb tree exhausted")))
+
+(defn amb
+  ([] fail)
+  ([expr] expr)
+  ([& exprs]
+   (let [fail-save @fail]
+     (cwcc
+      (fn [k-success]
+        (cwcc
+         (fn [k-failure]
+           (reset! fail (fn [] (k-failure nil)))
+           (k-success (fn [] exprs))))
+        (reset! fail fail-save)
+        @fail-save)))))
+
+(defn amb-possibilities  
+  [expr] 
+  (let [plist (atom [])] 
+    (amb (let [p expr] 
+           (swap! plist conj value) 
+           fail)
+         plist)))
